91d3f537f44e12a4c4cb7ac7e780c425170d2017
adityarizkydn <adityarizkydn@gmail.com>
2024-06-16 16:06:52 +0700

    Operasi Artmtika

---
+++ Operasi Aritmatika Phyton.py
@@ -0,0 +1,48 @@
+#--------------------------------------------------------
+#Operasi Aritmatika Phyton
+#--------------------------------------------------------
+
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+"""
+Output:
+
+16
+6
+55
+2
+2.2
+1
+161051
+"""
+
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+Output:
+
+False
+True
+False
+True
+False
+True
+"""
