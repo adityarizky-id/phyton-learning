374a0dcbbe120c6561cd20bd14f9c78527ed7ddb
adityarizkydn <adityarizkydn@gmail.com>
2024-06-17 06:05:54 +0700

    Ekspresi-learned

---
+++ operasi-logika.py
@@ -0,0 +1,20 @@
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+
+"""
+Output:
+
+True
+False
+False
+False
+"""
+
+x= "Belajar"
+y= "Phyton"
+result= x>y
+print(result)
+
