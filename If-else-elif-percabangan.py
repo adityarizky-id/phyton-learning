e928376c9816a26abc8e6be9ec17418681d5cb25
adityarizkydn <adityarizkydn@gmail.com>
2024-06-17 22:19:14 +0700

    percabangan

---
+++ If-else-elif-percabangan.py
@@ -0,0 +1,75 @@
+ketersediaan = "Daging ayam"
+
+if ketersediaan == "Daging ayam":
+    print("Ibu membeli dan memasak ayam")
+else:
+    print("Ibu membeli dan memasak tempe")
+
+"""
+Output:
+Ibu membeli dan memasak ayam
+"""
+
+score = 99
+
+if score == 100:
+    print("Nilai Anda sempurna!")
+else:
+    print("Nilai Anda tidak sempurna.")
+
+"""
+Output:
+Nilai Anda tidak sempurna.
+"""
+
+tinggi_badan = 120
+
+if tinggi_badan >=160:
+    print("Anda boleh menaiki roller coaster")
+else:
+    print("Anda tidak diperbolehkan menaiki roller coaster")
+
+"""
+Output:
+Anda tidak diperbolehkan menaiki roller coaster
+"""
+
+nilai = 65
+
+if nilai>=80:
+   print("Selamat! Anda mendapat nilai A")
+   print("Pertahankan!")
+elif nilai>=70:
+   print("Hore! Anda mendapat nilai B")
+   print("Tingkatkan!")
+elif nilai>=60:
+   print("Hmm.. Anda mendapat nilai C")
+   print("Ayo semangat!")
+else:
+   print("Waduh, Anda mendapat nilai D")
+   print("Yuk belajar lebih giat lagi!")
+
+
+"""
+Output:
+Hmm.. Anda mendapat nilai C
+Ayo semangat!
+"""
+
+nilai = 81
+perilaku = 'tidak baik'
+
+if nilai>=80 and perilaku == 'baik':
+   print("Selamat! Anda mendapat nilai A dan telah berkelakuan baik")
+   print("Pertahankan!")
+elif nilai>=80 and perilaku != 'baik':
+   print("Kamu mendapatkan nilai A, tetapi perilaku Anda kurang baik")
+   print("Perbaiki lagi ya!")
+else:
+   print("Yuk belajar lebih giat lagi!")
+
+"""
+Output:
+Kamu mendapatkan nilai A, tetapi perilaku Anda kurang baik
+Perbaiki lagi ya!
+"""
