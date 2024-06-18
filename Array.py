120b5e787167685bb801741a8a3847f5be293990
adityarizkydn <adityarizkydn@gmail.com>
2024-06-18 18:19:32 +0700

    fundamental array

---
+++ Array.py
@@ -0,0 +1,31 @@
+x = [1, 2, 3, 4, 5]
+print(x)
+
+"""
+Output:
+[1, 2, 3, 4, 5]
+"""
+
+import array
+
+x = array.array("i",[1, 2, 3, 4, 5])
+print(x)
+print(type(x))
+
+"""
+Output:
+array('i', [1, 2, 3, 4, 5])
+<class 'array.array'>
+"""
+
+siswa = [90, 100, 50, 80, 85, 45, 80, 75, 50, 60]
+
+print(siswa)
+print(siswa[0])
+
+
+"""
+Output:
+[90, 100, 50, 80, 85, 45, 80, 75, 50, 60]
+90
+"""
